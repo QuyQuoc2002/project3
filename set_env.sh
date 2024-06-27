363741bca88df52b8206e6fb243c612e850b3f9c
QuocPQ <quyquoc2002@gmail.com>
2024-06-27 08:12:10 +0700

    done mono to micro

--- set_env.sh
+++ set_env.sh
@@ -1,8 +1,8 @@
 export POSTGRES_USERNAME=admin
 export POSTGRES_PASSWORD=Shironeko02
-export POSTGRES_HOST=project3.cjkk0ensojz2.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=project3.cjpvqmrbflmv.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=project3
-export AWS_BUCKET=arn:aws:s3:::project3-141268504576-dev
+export AWS_BUCKET=project3-195234783967-dev-final
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=hello
